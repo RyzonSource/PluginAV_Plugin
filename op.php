55315f0c68eb57486d4582a24beaa7e09094b3a6
RyzonEnder <37978794+RyzonEnder@users.noreply.github.com>
2018-04-01 20:02:00 +0200

    Aggiunta La Funzione Con Il Comando /op

---
+++ op.php
@@ -0,0 +1,20 @@
+<?php
+function prom($chatID, $userID, $can_invite_users = true, $can_delete_messages = true, $can_restrict_members = true)
+{
+global $api;
+$args = array(
+'chat_id' => $chatID,
+'user_id' => $userID,
+'can_delete_messages' => $can_delete_messages
+);
+$r = new HttpRequest("get", "https://api.telegram.org/$api/promoteChatMember", $args);
+
+}
+
+if($msg=="/op")
+{
+	$sender = $update["message"]["from"]["username"];
+		$replyID = $update["message"]["from"]["id"];
+prom($chatID, $replyID);
+	sm($chatID, "Sei stato reso amministratore");
+}

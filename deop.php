93d9d3d0fe3006aefde4fd647944bc2db059c05d
RyzonEnder <37978794+RyzonEnder@users.noreply.github.com>
2018-04-02 09:41:48 +0200

    Creata la funzione e il comando per deoppare un utente
    Questa funzione vi deopperà dal gruppo.

---
+++ deop.php
@@ -0,0 +1,20 @@
+<?php
+function unprom($chatID, $userID, $can_invite_users = true, $can_delete_messages = true, $can_restrict_members = true)
+{
+global $api;
+$args = array(
+'chat_id' => $chatID,
+'user_id' => $userID,
+'can_delete_messages' => $can_delete_messages
+);
+$r = new HttpRequest("get", "https://api.telegram.org/$api/promoteChatMember", $args);
+
+}
+
+if($msg=="/deop")
+{
+	$sender = $update["message"]["from"]["username"];
+		$replyID = $update["message"]["from"]["id"];
+unprom($chatID, $replyID);
+	sm($chatID, "Sei stato reso utente");
+}
